03af36af78eed80b4a181cc9c200ece1825353a9
the-rohit-kumar <rohitkumar210103@gmail.com>
2023-11-09 20:09:49 +0530

    added Routes and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>APP</h1>
-  )
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Sign-In' element={<SignIn />} />
+          <Route path='/Sign-Up' element={<SignUp />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Profile' element={<Profile />} />
+
+        </Routes>
+    </BrowserRouter>
+  );
 }
