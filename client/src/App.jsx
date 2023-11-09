da42aac21e40a025abdc451fa034da75b38a1e4e
the-rohit-kumar <rohitkumar210103@gmail.com>
2023-11-09 20:29:25 +0530

    Added Header Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,14 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    {/** header */}
+    <Header/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Sign-In' element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+  return (
+    <h1 className='bg-slate-200'>
+        <div className='flex justify-between items-center max-w-full mx-auto p-3'>
+            <Link to='/'>
+            <h1 className='font-bold'>Auth-App</h1>
+            </Link>
+            <ul className='flex gap-4'>
+                 <Link to='/'>
+                <li>Home</li>
+                </Link>
+                <Link to='/about'>
+                <li>About</li>
+                </Link>
+                <Link to='/Sign-in'>
+                <li>Sign-In</li>
+                </Link>
+            </ul>
+        </div>
+    </h1>
+  )
+}
